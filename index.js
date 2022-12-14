a76e780c220ac3276f1620b3078a331ccb77a017
EarlofSmith <r1smith3333@gmail.com>
2022-12-13 21:27:06 -0700

    made and index instaled some modules started writing questions

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+      {
+        type: 'input',
+        message: 'What is your GitHub user name?',
+        name: 'github',
+      },
+      {
+        type: 'input',
+        message: 'Enter your Email',
+        name: 'questions',
+      },
+  ])
