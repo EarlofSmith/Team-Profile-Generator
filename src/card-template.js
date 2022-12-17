11c078406f3145424d1b2155005780aff406eb4f
EarlofSmith <r1smith3333@gmail.com>
2022-12-17 13:41:41 -0700

    got card template done

--- src/card-template.js
+++ src/card-template.js
@@ -21,6 +21,55 @@ let generateTeam = teamInfo => {
         let {name, id, email, position} = teamInfo[i]
 
         card +=`
-        `
+        <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">${name}</h5>
+                  <h6 class="card-subtitle mb-2">${position}</h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Employee id: ${id}</li>
+                  <li class="list-group-item">Email: <a href=mailto: ${email}>${email}</a></li>
+                  <li class="list-group-item">${choice}</li>
+                </ul>
+            </div>`
     }
+
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Porfile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.2.min.js" 
+    integrity="sha256-2krYZKh//PcchRtd+H+VyyQoZ/e3EcrkxhM8ycwASPA=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./style.css">
+
+
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="navbar-header">
+            <span class="navbar-brand">My Team</span>
+        </div>
+    </nav>
+
+    <main class = "container">
+        <div class="row">
+
+        ${card}    
+
+
+        </div>
+    </main>
+
+</body>
+</html>`
+
 }
+
+module.exports = generateTeam;
